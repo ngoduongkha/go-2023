71331c9bcffdd9427c8e4631050fcb7d77fbd7c8
Ngô Dương Kha <ngoduongkhakg2001@gmail.com>
2023-08-02 21:20:04 +0700

    add testcase

--- assignment-03/01-count-rectangles/main_test.go
+++ assignment-03/01-count-rectangles/main_test.go
@@ -43,6 +43,88 @@ func Test_countRectangles(t *testing.T) {
 			},
 			want: 5,
 		},
+		{
+			name: "Test 3",
+			args: args{
+				matrix: [][]int{
+					{0},
+				},
+			},
+			want: 0,
+		},
+		{
+			name: "Test 4",
+			args: args{
+				matrix: [][]int{
+					{},
+				},
+			},
+			want: 0,
+		},
+		{
+			name: "Test 5",
+			args: args{
+				matrix: [][]int{
+					{1},
+				},
+			},
+			want: 1,
+		},
+		{
+			name: "Test 6",
+			args: args{
+				matrix: [][]int{
+					{1, 1, 1, 1, 1, 1, 1},
+				},
+			},
+			want: 1,
+		},
+		{
+			name: "Test 7",
+			args: args{
+				matrix: [][]int{
+					{1, 0, 0, 0, 0, 0, 0},
+					{0, 1, 0, 0, 0, 0, 0},
+					{0, 0, 1, 0, 0, 0, 0},
+					{0, 0, 0, 1, 0, 0, 0},
+					{0, 0, 0, 0, 1, 0, 0},
+					{0, 0, 0, 0, 0, 1, 0},
+					{0, 0, 0, 0, 0, 0, 1},
+				},
+			},
+			want: 7,
+		},
+		{
+			name: "Test 8",
+			args: args{
+				matrix: [][]int{
+					{1, 1, 1},
+					{1, 1, 0},
+				},
+			},
+			want: 0,
+		},
+		{
+			name: "Test 9",
+			args: args{
+				matrix: [][]int{
+					{1, 0, 1},
+					{1, 0, 1},
+				},
+			},
+			want: 2,
+		},
+		{
+			name: "Test 10",
+			args: args{
+				matrix: [][]int{
+					{1},
+					{1},
+					{1},
+				},
+			},
+			want: 1,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
--- assignment-03/02-count-number/main_test.go
+++ assignment-03/02-count-number/main_test.go
@@ -39,6 +39,69 @@ func Test_countDifferentIntegers(t *testing.T) {
 			},
 			want: 4,
 		},
+		{
+			name: "Test 5",
+			args: args{
+				word: "abc123def456",
+			},
+			want: 2,
+		},
+		{
+			name: "Test 6",
+			args: args{
+				word: "1a2b3c4d5e6",
+			},
+			want: 6,
+		},
+		{
+			name: "Test 7",
+			args: args{
+				word: "12ab34cd56",
+			},
+			want: 3,
+		},
+		{
+			name: "Test 8",
+			args: args{
+				word: "leading0s0123456",
+			},
+			want: 2,
+		},
+		{
+			name: "Test 9",
+			args: args{
+				word: "001002003004005006",
+			},
+			want: 1,
+		},
+		{
+			name: "Test 10",
+			args: args{
+				word: "abc",
+			},
+			want: 0,
+		},
+		{
+			name: "Test 11",
+			args: args{
+				word: "",
+			},
+			want: 0,
+		},
+		{
+			name: "Test 12",
+			args: args{
+				word: "          ",
+			},
+			want: 0,
+		},
+		{
+			name: "Test 13",
+			args: args{
+				word: "  123 2 sx 21 c212x",
+			},
+			want: 4,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
